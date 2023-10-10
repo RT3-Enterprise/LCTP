56db7020bde64a67df3cd0b6ce0552e52441ed6c
IlkerOnay <97039354+IlkerOnay@users.noreply.github.com>
2023-10-10 12:03:03 +0200

    Add files via upload

--- Code capture de trame/script.py
+++ Code capture de trame/script.py
@@ -9,13 +9,14 @@ def sniff_dhcp_packets(interface):  # definition de la fonction sniff_dhcp_packe
 def process_dhcp_packet(packet): # definition de la fonction process_dhcp_packet d'argument packet
     if packet.haslayer(scapy.DHCP) and packet[scapy.DHCP].options[0][1] == 5: # haslayer permet de vérifier ci le packet capturé est bien un packet DHCP de type ACK grace à  "== 5"
         print("----- Trame DHCP Capturée -----") # permet de différencier les trames en marquant le début du packet
-        print(f"Source IP: {packet[scapy.IP].src}, Destination IP: {packet[scapy.IP].dst}") #
-        print(f"Source MAC: {packet[scapy.Ether].src}, Destination MAC: {packet[scapy.Ether].dst}")
-        print("Lease Time: ", packet[scapy.DHCP].options[2][1])
-        print("Subnet Mask: ", packet[scapy.DHCP].options[3][1])
-        print("Server IP: ", packet[scapy.DHCP].options[4][1])
-        print("DNS IP: ", packet[scapy.DHCP].options[5][1])
-        print("Domain Name: ", packet[scapy.DHCP].options[6][1])
+        print(f"Source IP: {packet[scapy.IP].src}, Destination IP: {packet[scapy.IP].dst}") # Permet de renvoyer l'IP source et destination
+        print(f"Source MAC: {packet[scapy.Ether].src}, Destination MAC: {packet[scapy.Ether].dst}")# Permet de renvoyer la MAC Source et Destination
+        print("Opcode: ", packet[scapy.DHCP].options[0][1]) # Permet de renvoyer l'Opcode qui détermine la nature du paquet ACK, REQUEST , OFFER .....
+        print("Lease Time: ", packet[scapy.DHCP].options[2][1])# Permet de renvoyer le temps du bail restant en seconde 
+        print("Subnet Mask: ", packet[scapy.DHCP].options[3][1])# Permet de renvoyer le masque de sous-réseaux
+        print("Routeur: ", packet[scapy.DHCP].options[4][1])# Permet de renvoyer l'IP du routeur
+        print("Domain Name: ", packet[scapy.DHCP].options[5][1])# Permet de renvoyer le nom de domain
+        print("DNS Ip: ", packet[scapy.DHCP].options[6][1]) # Permet de renvoyer l'IP du DNS
         print("---------------------------------") #permet de marquer la fin du packet
 if __name__ == "__main__":
     interface = "eth0"  # Remplacez par le nom de votre interface réseau (par exemple, "eth0")

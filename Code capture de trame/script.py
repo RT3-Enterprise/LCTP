baadd7cad8fc0776d51fb5475b47df482af6056e
IlkerOnay <97039354+IlkerOnay@users.noreply.github.com>
2023-10-12 12:17:34 +0200

    Add files via upload
    Création de l'objet pour rassembler toutes les variables

--- Code capture de trame/script.py
+++ Code capture de trame/script.py
@@ -1,5 +1,19 @@
 import scapy.all as scapy # import de la bibliothèque scapy
 type_list=["","Discover","Offer","Request","Decline","Ack","Nak","Release","Inform"] # On identifie grâce au numméro d'Opcode la nature depuis cette liste
+
+class DhcpPacketInfo:
+    def __init__(self, trame_type, source_ip, destination_ip, source_mac, destination_mac, lease_time, subnet_mask, routeur, domain_name, dns_ip):
+        self.trame_type = trame_type
+        self.source_ip = source_ip
+        self.destination_ip = destination_ip
+        self.source_mac = source_mac
+        self.destination_mac = destination_ip
+        self.lease_time = lease_time
+        self.subnet_mask = subnet_mask
+        self.routeur = routeur
+        self.domain_name = domain_name
+        self.dns_ip = dns_ip
+
 def sniff_dhcp_packets(interface):  # definition de la fonction sniff_dhcp_packets d'argument interface, il capture les packets dhcp
     try:    # un bloc d'essai pour gérer les exceptions
         scapy.sniff(iface=interface, store=False, prn=process_dhcp_packet, filter="udp and port 67 or port 68") # fonction sniff de scapy pour capturer les packets , iface --> interface , store --> stockage en mémoire True Or False , prn --> fonction qui sera apelle pour chaque paquet capturé, filtre udp 67 donc server DHCP et 68 client DHCP
@@ -10,19 +24,18 @@ def process_dhcp_packet(packet): # definition de la fonction process_dhcp_packet
     if packet.haslayer(scapy.DHCP): # haslayer permet de vérifier ci le packet capturé est bien un packet DHCP de type ACK grace à  "== 5"
         type=packet[scapy.DHCP].options[0][1]# Permet de renvoyer l'Opcode qui détermine la nature du paquet et de la stocker dans la variable "type"
         trame_type = type_list[type]
-        Source_IP= packet[scapy.IP].src
-        Destination_IP= packet[scapy.IP].dst
-        Source_MAC= packet[scapy.Ether].src
-        Destination_MAC= packet[scapy.Ether].dst
-        Lease_Time=packet[scapy.DHCP].options[2][1]
-        Subnet_Mask=packet[scapy.DHCP].options[3][1]
-        Routeur=packet[scapy.DHCP].options[4][1]
-        Domain_Name=packet[scapy.DHCP].options[5][1]
-        DNS_Ip= packet[scapy.DHCP].options[6][1]
-        return trame_type,Source_IP,Destination_IP,Source_MAC,Destination_MAC,Lease_Time,Subnet_Mask,Routeur,DNS_Ip
-
-
-
+        source_ip= packet[scapy.IP].src
+        destination_ip= packet[scapy.IP].dst
+        source_mac= packet[scapy.Ether].src
+        destination_mac= packet[scapy.Ether].dst
+        lease_time=packet[scapy.DHCP].options[2][1]
+        subnet_mask=packet[scapy.DHCP].options[3][1]
+        routeur=packet[scapy.DHCP].options[4][1]
+        domain_name=packet[scapy.DHCP].options[5][1]
+        dns_ip=packet[scapy.DHCP].options[6][1]
+        dhcp_info = DhcpPacketInfo(trame_type, source_ip, destination_ip, source_mac, destination_mac, lease_time, subnet_mask, routeur, domain_name, dns_ip)
+        print(dhcp_info.trame_type)
+        return dhcp_info
 
 
 

3990cf8b680a78e3839116f642dad0e8c92e0218
IlkerOnay <97039354+IlkerOnay@users.noreply.github.com>
2023-12-20 11:36:15 +0100

    Add files via upload

--- Code capture de trame/script.py
+++ Code capture de trame/script.py
@@ -1,7 +1,7 @@
 import scapy.all as scapy # import de la bibliothèque scapy
 type_list=["","Discover","Offer","Request","Decline","Ack","Nak","Release","Inform"] # On identifie grâce au numméro d'Opcode la nature depuis cette liste
 
-class DhcpPacketInfo:
+class DhcpPacketInfo: #initialisation de l'object 
     def __init__(self, trame_type, source_ip, destination_ip, source_mac, destination_mac, lease_time, subnet_mask, routeur, domain_name, dns_ip):
         self.trame_type = trame_type
         self.source_ip = source_ip
@@ -21,9 +21,9 @@ def sniff_dhcp_packets(interface):  # definition de la fonction sniff_dhcp_packe
         print("Arrêt de la capture.") # le script nous dis alors que la capture s'arrête
 
 def process_dhcp_packet(packet): # definition de la fonction process_dhcp_packet d'argument packet
-    if packet.haslayer(scapy.DHCP): # haslayer permet de vérifier ci le packet capturé est bien un packet DHCP de type ACK grace à  "== 5"
+    if packet.haslayer(scapy.DHCP):
         type=packet[scapy.DHCP].options[0][1]# Permet de renvoyer l'Opcode qui détermine la nature du paquet et de la stocker dans la variable "type"
-        trame_type = type_list[type]
+        trame_type = type_list[type] # Le nom des variables ci-dessous permet de savoir ce que l'on récupére
         source_ip= packet[scapy.IP].src
         destination_ip= packet[scapy.IP].dst
         source_mac= packet[scapy.Ether].src
@@ -33,7 +33,7 @@ def process_dhcp_packet(packet): # definition de la fonction process_dhcp_packet
         routeur=packet[scapy.DHCP].options[4][1]
         domain_name=packet[scapy.DHCP].options[5][1]
         dns_ip=packet[scapy.DHCP].options[6][1]
-        dhcp_info = DhcpPacketInfo(trame_type, source_ip, destination_ip, source_mac, destination_mac, lease_time, subnet_mask, routeur, domain_name, dns_ip)
+        dhcp_info = DhcpPacketInfo(trame_type, source_ip, destination_ip, source_mac, destination_mac, lease_time, subnet_mask, routeur, domain_name, dns_ip) # renvoie de l'objet dhcp_info pour la récupération sur la bdd
         print(dhcp_info.trame_type)
         return dhcp_info
 

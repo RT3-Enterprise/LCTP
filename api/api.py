aca9242acdc182b5ac6d8715e83d9a7f1df0ae44
Nicolas LE LAN <nicolaslelan13@gmail.com>
2024-01-23 14:33:24 +0100

    test

--- api/api.py
+++ api/api.py
@@ -7,22 +7,21 @@ client = db.client()
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/', methods=['GET'])
-def home():
+@app.route('/', methods=['GET']) # route de la page d'accueil
+def home(): # fonction de la page d'accueil
  return '''<h1>API LCTP</h1>
 <p>Capture et Analise de trame</p>'''
-# A route to return all of the available entries in our catalog.
 
-@app.route('/api/v1/resources/packet/all', methods=['GET'])
-def api_get_packet():
+@app.route('/api/v1/resources/packet/all', methods=['GET']) # route pour récupérer tous les packets
+def api_get_packet():   # fonction pour récupérer tous les packets
     raw,trame = db.get_db(client)
     L = [raw,trame]
     return flask.jsonify(L)
 
-@app.route('/api/v1/resources/packet', methods=['GET'])
-def api_get_packet_id():
-    raw,trame = db.get_db(client)
-    if 'id' in flask.request.args:
+@app.route('/api/v1/resources/packet', methods=['GET']) # route pour récupérer un packet en fonction de son id
+def api_get_packet_id(): # fonction pour récupérer un packet
+    raw,trame = db.get_db(client) # on récupère les packets
+    if 'id' in flask.request.args: # Vérification de id dans les arguments
         id = flask.request.args['id']
     else:
         return "Error: No id field provided. Please specify an id."
@@ -35,64 +34,64 @@ def api_get_packet_id():
             results.append(tr)
     return flask.jsonify(results)
 
-@app.route('/api/v1/resources/packet', methods=['POST'])
-def api_post_packet():
-    content = flask.request.json
-    packet = db.json_to_packet(content)
-    db.insert_packet(client, packet)
+@app.route('/api/v1/resources/packet', methods=['POST']) # route pour poster un packet
+def api_post_packet(): # fonction pour poster un packet
+    content = flask.request.json # on récupère le contenu de la requête
+    packet = db.json_to_packet(content) # on transforme le contenu en packet
+    db.insert_packet(client, packet) # on insère le packet dans la base de donnée
     return 'OK'
 
-@app.route('/api/v1/resources/packet', methods=['DELETE'])
-def api_delete_packet():
-    content = flask.request.json
-    packet = db.json_to_packet(content)
-    db.delete_packet(client, packet)
+@app.route('/api/v1/resources/packet', methods=['DELETE']) # route pour supprimer un packet
+def api_delete_packet(): # fonction pour supprimer un packet
+    content = flask.request.json # on récupère le contenu de la requête
+    packet = db.json_to_packet(content) # on transforme le contenu en packet
+    db.delete_packet(client, packet)    # on supprime le packet dans la base de donnée
     return 'OK'
 
-@app.route('/api/v1/resources/packet', methods=['PUT'])
-def api_put_packet():
-    content_json = flask.request.json
-    content = json.loads(content_json)
-    packet = db.json_to_packet(content['json'])
-    new_packet = db.json_to_packet(content['json_new'])
-    db.change_packet(client, packet, new_packet)
+@app.route('/api/v1/resources/packet', methods=['PUT']) # route pour modifier un packet
+def api_put_packet(): # fonction pour modifier un packet
+    content_json = flask.request.json # on récupère le contenu de la requête
+    content = json.loads(content_json) # on transforme le contenu en json
+    packet = db.json_to_packet(content['json']) # on transforme le json en packet
+    new_packet = db.json_to_packet(content['json_new']) # on transforme le json en packet
+    db.change_packet(client, packet, new_packet) # on modifie le packet dans la base de donnée
     return 'OK'
 
-@app.route('/api/v1/resources/trame/all', methods=['GET'])
-def api_all_trame():
-    _,trame = db.get_db(client)
-    return flask.jsonify(trame)
+@app.route('/api/v1/resources/trame/all', methods=['GET']) # route pour récupérer toutes les trames
+def api_all_trame(): # fonction pour récupérer toutes les trames
+    _,trame = db.get_db(client) # on récupère les trames
+    return flask.jsonify(trame) # on retourne les trames
 
-@app.route('/api/v1/resources/trame', methods=['GET'])
-def api_filter():
-    _,trame = db.get_db(client)
-    args = flask.request.args
+@app.route('/api/v1/resources/trame', methods=['GET']) # route pour récupérer une trame en fonction de son id
+def api_filter(): # fonction pour récupérer une trame
+    _,trame = db.get_db(client) # on récupère les trames
+    args = flask.request.args # on récupère les arguments de la requête
     results = []
-    for tr in trame:
+    for tr in trame: # on parcours les trames
         flag = False
         only = True
-        for key in args:
-            if key in tr:
-                if tr[key] == args[key]:
+        for key in args: # on parcours les arguments
+            if key in tr: # on vérifie si l'argument est dans la trame
+                if tr[key] == args[key]: # on vérifie si la valeur de l'argument est égale à la valeur de la trame
                     flag = True
                 else: 
-                    only = False
+                    only = False 
         if 'only' in args:
-            results.append(tr) if only else None
+            results.append(tr) if only else None # on ajoute la trame si only est vrai sinon on ajoute rien
         else:
-            results.append(tr) if flag else None
+            results.append(tr) if flag else None # on ajoute la trame si flag est vrai sinon on ajoute rien
                         
-    return flask.jsonify(results)
+    return flask.jsonify(results) # on retourne les trames
 
-@app.route('/api/v1/resources/raw/all', methods=['GET'])
-def api_all_raw():
-    raw,_ = db.get_db(client)
-    return flask.jsonify(raw)
+@app.route('/api/v1/resources/raw/all', methods=['GET']) # route pour récupérer tous les raw
+def api_all_raw(): # fonction pour récupérer tous les raw
+    raw,_ = db.get_db(client) # on récupère les raw
+    return flask.jsonify(raw) # on retourne les raw
 
-@app.route('/api/v1/resources/raw', methods=['GET'])
-def api_id_raw():
-    raw,_ = db.get_db(client)
-    if 'id' in flask.request.args:
+@app.route('/api/v1/resources/raw', methods=['GET']) # route pour récupérer un raw en fonction de son id
+def api_id_raw(): # fonction pour récupérer un raw
+    raw,_ = db.get_db(client) # on récupère les raw
+    if 'id' in flask.request.args: # on vérifie si id est dans les arguments
         id = flask.request.args['id']
     else:
         return "Error: No id field provided. Please specify an id."
@@ -100,52 +99,52 @@ def api_id_raw():
     for ra in raw:
         if ra['_id'] == id:
             results.append(ra)
-    return flask.jsonify(results)
+    return flask.jsonify(results) # on retourne les raw
 
-@app.route('/api/v1/resources/baux/all', methods=['GET'])
-def api_all_baux():
-    baux = db.get_baux(client)
-    return flask.jsonify(baux)
+@app.route('/api/v1/resources/baux/all', methods=['GET']) # route pour récupérer tous les baux
+def api_all_baux(): # fonction pour récupérer tous les baux
+    baux = db.get_baux(client) # on récupère les baux
+    return flask.jsonify(baux) # on retourne les baux
 
-@app.route('/api/v1/resources/baux', methods=['GET'])
-def api_baux_filter():
-    baux = db.get_baux(client)
-    args = flask.request.args
+@app.route('/api/v1/resources/baux', methods=['GET']) # route pour récupérer un baux en fonction de son id
+def api_baux_filter(): # fonction pour récupérer un baux
+    baux = db.get_baux(client) # on récupère les baux
+    args = flask.request.args # on récupère les arguments de la requête
     results = []
-    for bail in baux:
+    for bail in baux: # on parcours les baux
         flag = False
         only = True
-        for key in args:
-            if key in ba:
-                if ba[key] == args[key]:
+        for key in args: # on parcours les arguments
+            if key in ba: # on vérifie si l'argument est dans le baux
+                if ba[key] == args[key]: # on vérifie si la valeur de l'argument est égale à la valeur du baux
                     flag = True
                 else: 
                     only = False
         if 'only' in args:
-            results.append(ba) if only else None
+            results.append(ba) if only else None # on ajoute le baux si only est vrai sinon on ajoute rien
         else:
-            results.append(ba) if flag else None
+            results.append(ba) if flag else None # on ajoute le baux si flag est vrai sinon on ajoute rien
                         
-    return flask.jsonify(results)
+    return flask.jsonify(results) # on retourne les baux
 
-def api_post_baux():
-    content = flask.request.json
-    baux = db.json_to_baux(content)
-    db.insert_baux(client, baux)
+def api_post_baux(): # fonction pour poster un baux
+    content = flask.request.json # on récupère le contenu de la requête
+    baux = db.json_to_baux(content) # on transforme le contenu en baux
+    db.insert_baux(client, baux) # on insère le baux dans la base de donnée
     return 'OK'
 
-def api_delete_baux():
-    content = flask.request.json
-    baux = db.json_to_baux(content)
-    db.delete_baux(client, baux)
+def api_delete_baux(): # fonction pour supprimer un baux
+    content = flask.request.json # on récupère le contenu de la requête
+    baux = db.json_to_baux(content) # on transforme le contenu en baux
+    db.delete_baux(client, baux) # on supprime le baux dans la base de donnée
     return 'OK'
 
 def api_put_baux():
-    content_json = flask.request.json
-    content = json.loads(content_json)
-    baux = db.json_to_baux(content['json'])
-    new_baux = db.json_to_baux(content['json_new'])
-    db.change_baux(client, baux, new_baux)
+    content_json = flask.request.json # on récupère le contenu de la requête
+    content = json.loads(content_json) # on transforme le contenu en json 
+    baux = db.json_to_baux(content['json']) # on transforme le json en baux
+    new_baux = db.json_to_baux(content['json_new']) # on transforme le json en baux
+    db.change_baux(client, baux, new_baux) # on modifie le baux dans la base de donnée
     return 'OK'
 
 if __name__ == "__main__":
--- api/request.py
+++ api/request.py
@@ -6,32 +6,32 @@ import utils
 API_URL = os.getenv('API_URL', 'http://localhost:5000/api/v1/resources/')
 
 
-def get(url):
+def get(url): # Fonction pour faire des requêtes GET
     return requests.get(url).json()
 
-def get_packet_all():
+def get_packet_all(): # Fonction pour récupérer tous les packets
     return get(API_URL + '/packet/all')
 
-def get_raw_all():
+def get_raw_all(): # Fonction pour récupérer tous les raw
     return get(API_URL + '/raw/all')
 
-def get_trame_flitered(filter, only=False):
+def get_trame_flitered(filter, only=False): # Fonction pour récupérer les trames filtrées
     if only:
         filter['only'] = 'True'
     return requests.get(API_URL + '/trame', params=filter).json()
 
-def get_trame_all():
+def get_trame_all(): # Fonction pour récupérer toutes les trames
     return get(API_URL + '/trame/all')
 
-def post_packet(packet:utils.Packet):
+def post_packet(packet:utils.Packet): # Fonction pour poster un packet
     json_packet = packet.to_json_all()
     return requests.post(API_URL + '/packet', json=json_packet)
 
-def delete_packet(packet:utils.Packet):
+def delete_packet(packet:utils.Packet): # Fonction pour supprimer un packet
     json_packet = packet.to_json_all()
     return requests.delete(API_URL + '/packet', json=json_packet)
 
-def put_packet(packet:utils.Packet, new_packet:utils.Packet):
+def put_packet(packet:utils.Packet, new_packet:utils.Packet): # Fonction pour modifier un packet
     json_packet = packet.to_json_all()
     json_new_packet = new_packet.to_json_all()
     data = {
@@ -39,6 +39,44 @@ def put_packet(packet:utils.Packet, new_packet:utils.Packet):
         'json_new': json_new_packet
     }
     return requests.put(API_URL + '/packet', json=data)
+
+def get_baux_all(): # Fonction pour récupérer tous les baux
+    return get(API_URL + '/baux/all')
+
+def get_baux_filtered(filter): # Fonction pour récupérer les baux filtrés
+    return requests.get(API_URL + '/baux', params=filter).json()
+
+def ip_in_baux(ip): # Fonction pour vérifier si une ip est dans les baux
+    return get_baux_filtered({'IP': ip}) if len(get_baux_filtered({'IP': ip})) > 0 else False
+
+def post_baux(baux:utils.Baux): # Fonction pour poster un baux
+    json_baux = baux.to_json()
+    return requests.post(API_URL + '/baux', json=json_baux)
+
+def put_baux(baux:utils.Baux, new_baux:utils.Baux): # Fonction pour modifier un baux
+    # A FAIRE
+    json_baux = baux.to_json()
+    json_new_baux = new_baux.to_json()
+    data = {
+        'json': json_baux,
+        'json_new': json_new_baux
+    }
+    return requests.put(API_URL + '/baux', json=data)
+
+def put_baux_ip(ip) # Fonction pour modifier un baux en fonction de son ip
+    # A FAIRE
+    content = ip_in_baux(ip)
+    if content:
+        bail = json_to_baux(content)
+        
+    else:
+        return post_baux(bail)
+
+def delete_baux(baux:utils.Baux): # Fonction pour supprimer un baux
+    json_baux = baux.to_json()
+    return requests.delete(API_URL + '/baux', json=json_baux)
+
+# Tests
 
 p = utils.Packet('RAW1', 'SRC1', 'DST1', 'MAC1', 'TYPE1', 'BAIL1', 'MASQUE1', 'DHCP1', 'DN1', 'DNS1', 'ROUTER1')
 post_packet(p)

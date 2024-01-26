02b297a1bf7aca3bdf05e10a3380ac3a4422dbf2
IlkerOnay <97039354+IlkerOnay@users.noreply.github.com>
2024-01-26 15:24:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,12 @@
 # Mise à jour et installation des paquets
 apt-get update &&
 apt-get upgrade -y &&
-apt-get install -y git libgl1-mesa-glx libxcb-icccm4 nano iputils-ping python3 python3-pip iproute2 libglib2.0-0
+apt-get install -y gnupg git libgl1-mesa-glx libxcb-icccm4 nano iputils-ping python3 python3-pip iproute2 libglib2.0-0
 
 # Installation des modules Python
 pip3 install pymongo scapy requests flask PyQt5 matplotlib waitress
 
 # Installation de gnupg et ajout de la clé MongoDB
-sudo apt-get install gnupg -y
 wget -qO - https://www.mongodb.org/static/pgp/server-${MONGO_VERSION}.asc | sudo apt-key add -
 cd /etc/apt/sources.list.d/
 sudo touch mongodb-org-${MONGO_VERSION}.list

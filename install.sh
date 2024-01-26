43e9a29352530d1f00f5e69d77a326b77363ea21
IlkerOnay <97039354+IlkerOnay@users.noreply.github.com>
2024-01-26 15:28:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ apt-get install -y gnupg git libgl1-mesa-glx libxcb-icccm4 nano iputils-ping pyt
 # Installation des modules Python
 pip3 install pymongo scapy requests flask PyQt5 matplotlib waitress
 
-# Installation de gnupg et ajout de la clé MongoDB
+# Ajout de la clé MongoDB
 wget -qO - https://www.mongodb.org/static/pgp/server-${MONGO_VERSION}.asc | sudo apt-key add -
 cd /etc/apt/sources.list.d/
 sudo touch mongodb-org-${MONGO_VERSION}.list
